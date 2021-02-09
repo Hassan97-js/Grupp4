8d9cb64e8cc318cc198407015b0de3176474170a
Noel Andersson <noel.anderssonkohler@hotmail.com>
2021-02-09 20:32:12 +0100

    - Added component LandingPage.vue - Added component Reviews.vue - Linked first three categories to respective router-pages - Added LandingPage and Reviews to Home.vue

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,61 @@
+<template>
+<div class="landingPage">
+    <div class="categoryDeck">
+        <!-- tre kategori-cards -->
+        <b-card-group deck>
+            <b-card title="Dam" img-src="https://picsum.photos/300/300/?image=41" img-alt="Image" img-top>
+            <b-link to="../views/Dam" class="stretched-link"></b-link>
+            </b-card>
+
+            <b-card title="Herr" img-src="https://picsum.photos/300/300/?image=41" img-alt="Image" img-top>
+            <b-link to="../views/Herr" class="stretched-link"></b-link>
+            </b-card>
+
+            <b-card title="Barn" img-src="https://picsum.photos/300/300/?image=41" img-alt="Image" img-top>
+            <b-link to="../views/Barn" class="stretched-link"></b-link>
+            </b-card>
+        </b-card-group>
+    </div>
+
+    <!-- jumbotron med rea -->
+    <div class="saleJumbotron">
+        <b-jumbotron header="Sale" lead="View all items on Sale">
+            <b-button variant="primary" href="#">Shop Sale</b-button>
+        </b-jumbotron>
+    </div>
+    
+    <!-- tre kategori-cards -->
+    <div class="categoryDeck">
+        <b-card-group deck>
+            <b-card title="Jackor" img-src="https://picsum.photos/300/300/?image=41" img-alt="Image" img-top>
+            </b-card>
+
+            <b-card title="T-shirts" img-src="https://picsum.photos/300/300/?image=41" img-alt="Image" img-top>
+            </b-card>
+
+            <b-card title="Accessoarer" img-src="https://picsum.photos/300/300/?image=41" img-alt="Image" img-top>
+            </b-card>
+        </b-card-group>
+    </div>
+</div>
+</template>
+<script>
+export default {
+    name: 'LandingPage'
+}
+</script>
+
+<style scoped>
+div.landingPage{
+    padding: 20px;
+}
+.saleJumbotron{
+    padding: 20px;
+}
+.jumbotron{
+    margin: 0px;
+}
+.categoryDeck{
+    padding: 20px;
+}
+</style>
---
+++ src/components/Reviews.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="categoryDeck">
+        <b-card-group deck>
+            <b-card class="reviewCard">
+                <b-img v-bind="mainProps" rounded="circle" alt="Circle image" fluid></b-img>
+                <b-card-text>
+                Some review text to show how the company is.
+                </b-card-text>
+
+                <b-card-text>Name or something of the customer.</b-card-text>
+                </b-card>
+
+            <b-card class="reviewCard">
+                <b-img v-bind="mainProps" rounded="circle" alt="Circle image" fluid></b-img>
+                <b-card-text>
+                Some review text to show how the company is.
+                </b-card-text>
+
+                <b-card-text>Name or something of the customer.</b-card-text>
+            </b-card>
+        
+            <b-card class="reviewCard">
+                <b-img v-bind="mainProps" rounded="circle" alt="Circle image" fluid></b-img>
+                <b-card-text>
+                Some review text to show how the company is.
+                </b-card-text>
+
+                <b-card-text>Name or something of the customer.</b-card-text>
+            </b-card>
+        </b-card-group>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+      return {
+        mainProps: { blank: true, blankColor: '#777', width: 150, height: 150, class: 'm1' }
+      }
+    },
+     name: 'Reviews'
+}
+</script>
+
+<style scoped>
+.categoryDeck{
+padding: 0px 100px
+}
+p.card-text{
+    padding: 15px;
+}
+</style>
