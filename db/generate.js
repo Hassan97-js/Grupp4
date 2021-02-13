d824efb882a700847df0c0f685acef21331f3a8d
Hassan97-js <hassansyr58@gmail.com>
2021-02-13 10:42:04 +0000

    I did: * Fixed problem with running two npm scripts at the same time * I installed npm-run-all package * Added a json-vue-serve npm script to run the vue server and json-server at the same time * I didn't uninstall concurrently npm package

--- db/generate.js
+++ db/generate.js
@@ -1,18 +1,18 @@
+// jshint esversion:6
+
 module.exports = function() {
-    const _ = require("lodash")
+  const _ = require("lodash");
 
-    return {
-        products: _.times(50, function(n) {
-            return {
-                //Tänker att vi med v-if kan koppla bilder till?
-                id: n+1,
-                productType: _.sample(["tshirt", "trousers", "hoodie","assec" ]),
-                productTarget: _.sample(["women", "men", "kids"]),
-                onSale: _.sample([true, false]),
-                price: _.sample([99, 149, 199, 299])
-            }
-        })
-
-        
-    }
-}
+  return {
+    products: _.times(50, function(n) {
+      return {
+        //Tänker att vi med v-if kan koppla bilder till?
+        id: n + 1,
+        productType: _.sample([ "tshirt", "trousers", "hoodie", "assec" ]),
+        productTarget: _.sample([ "women", "men", "kids" ]),
+        onSale: _.sample([ true, false ]),
+        price: _.sample([ 99, 149, 199, 299 ])
+      };
+    })
+  };
+};
