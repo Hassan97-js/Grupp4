07ddc9787370f3a0ac6274e7e73f647fc8565db0
Gremlet <57172450+Gremlet@users.noreply.github.com>
2021-02-16 20:49:13 +0100

    Product Gallery - filtered array and used props to display products in all categories

--- src/views/Barn.vue
+++ src/views/Barn.vue
@@ -1,7 +1,32 @@
 <template>
-  <div>
-    <h1>Barn</h1>
-  </div>
+    <div>
+        <h1>Barn</h1>
+        <product-gallery :products="kids"></product-gallery>
+    </div>
 </template>
+
+<script>
+    import ProductGallery from '@/components/ProductGallery.vue'
+    export default {
+        components: { ProductGallery },
+        name: 'Barn',
+        created() {
+            fetch('http://localhost:3000/products')
+                .then(response => {
+                    return response.json()
+                })
+                .then(data => {
+                    this.kids = data.filter(function(category) {
+                        return category.productTarget === 'kids'
+                    })
+                })
+        },
+        data() {
+            return {
+                kids: []
+            }
+        }
+    }
+</script>
 
 <style scoped lang="scss"></style>
--- src/views/Herr.vue
+++ src/views/Herr.vue
@@ -1,7 +1,31 @@
 <template>
-  <div>
-     <h1>Herr</h1>
-  </div>
+    <div>
+        <h1>Herr</h1>
+        <product-gallery :products="men"></product-gallery>
+    </div>
 </template>
 
+<script>
+    import ProductGallery from '@/components/ProductGallery.vue'
+    export default {
+        components: { ProductGallery },
+        name: 'Herr',
+        created() {
+            fetch('http://localhost:3000/products')
+                .then(response => {
+                    return response.json()
+                })
+                .then(data => {
+                    this.men = data.filter(function(category) {
+                        return category.productTarget === 'men'
+                    })
+                })
+        },
+        data() {
+            return {
+                men: []
+            }
+        }
+    }
+</script>
 <style scoped lang="scss"></style>
