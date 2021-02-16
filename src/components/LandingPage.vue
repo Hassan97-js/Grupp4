45702d9075c1fd09d9e9b915e154f41a71baf0ba
Noel Andersson <noel.anderssonkohler@hotmail.com>
2021-02-16 09:32:10 +0100

    Product gallery - Added ProductGallery.vue - Put Product Gallery-component into "Barn" - Created grid layout with ProductType, Price, OnSale-badge, ProductCart and Favorite-icon

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,64 +1,94 @@
 <template>
-<div class="landingPage">
-    <div class="categoryDeck">
+    <div class="landingPage">
+        <div class="categoryDeck">
+            <!-- tre kategori-cards -->
+            <b-row>
+                <b-card
+                    title="Women"
+                    img-src="https://picsum.photos/300/300/?image=41"
+                    img-alt="Image"
+                    img-top
+                >
+                    <b-link to="../views/Dam" class="stretched-link"></b-link>
+                </b-card>
+
+                <b-card
+                    title="Men"
+                    img-src="https://picsum.photos/300/300/?image=41"
+                    img-alt="Image"
+                    img-top
+                >
+                    <b-link to="../views/Herr" class="stretched-link"></b-link>
+                </b-card>
+
+                <b-card
+                    title="Children"
+                    img-src="https://picsum.photos/300/300/?image=41"
+                    img-alt="Image"
+                    img-top
+                >
+                    <b-link to="../views/Barn" class="stretched-link"></b-link>
+                </b-card>
+            </b-row>
+        </div>
+
+        <!-- jumbotron med rea -->
+        <div class="saleJumbotron">
+            <b-jumbotron header="Sale" lead="View all items on Sale">
+                <b-button variant="primary" href="#">Shop Sale</b-button>
+            </b-jumbotron>
+        </div>
+
         <!-- tre kategori-cards -->
-        <b-row>
-            <b-card title="Women" img-src="https://picsum.photos/300/300/?image=41" img-alt="Image" img-top>
-            <b-link to="../views/Dam" class="stretched-link"></b-link>
-            </b-card>
+        <div class="categoryDeck">
+            <b-row>
+                <b-card
+                    title="Jackets"
+                    img-src="https://picsum.photos/300/300/?image=41"
+                    img-alt="Image"
+                    img-top
+                >
+                </b-card>
 
-            <b-card title="Men" img-src="https://picsum.photos/300/300/?image=41" img-alt="Image" img-top>
-            <b-link to="../views/Herr" class="stretched-link"></b-link>
-            </b-card>
+                <b-card
+                    title="T-shirts"
+                    img-src="https://picsum.photos/300/300/?image=41"
+                    img-alt="Image"
+                    img-top
+                >
+                </b-card>
 
-            <b-card title="Children" img-src="https://picsum.photos/300/300/?image=41" img-alt="Image" img-top>
-            <b-link to="../views/Barn" class="stretched-link"></b-link>
-            </b-card>
-        </b-row>
+                <b-card
+                    title="Accessories"
+                    img-src="https://picsum.photos/300/300/?image=41"
+                    img-alt="Image"
+                    img-top
+                >
+                </b-card>
+            </b-row>
+        </div>
     </div>
-
-    <!-- jumbotron med rea -->
-    <div class="saleJumbotron">
-        <b-jumbotron header="Sale" lead="View all items on Sale">
-            <b-button variant="primary" href="#">Shop Sale</b-button>
-        </b-jumbotron>
-    </div>
-    
-    <!-- tre kategori-cards -->
-    <div class="categoryDeck">
-        <b-row>
-            <b-card title="Jackets" img-src="https://picsum.photos/300/300/?image=41" img-alt="Image" img-top>
-            </b-card>
-
-            <b-card title="T-shirts" img-src="https://picsum.photos/300/300/?image=41" img-alt="Image" img-top>
-            </b-card>
-
-            <b-card title="Accessories" img-src="https://picsum.photos/300/300/?image=41" img-alt="Image" img-top>
-            </b-card>
-        </b-row>
-    </div>
-</div>
 </template>
 <script>
-export default {
-    name: 'LandingPage'
-}
+    export default {
+        name: 'LandingPage'
+    }
 </script>
 
 <style scoped>
-div.landingPage{
-    padding: 20px;
-}
-.saleJumbotron{
-    padding: 20px;
-}
-.jumbotron{
-    margin: 0px;
-}
-.categoryDeck{
-    padding: 20px;
-}
-div.row{
-    justify-content: space-evenly;
-}
+    div.landingPage {
+        padding: 20px;
+    }
+    .saleJumbotron {
+        padding: 20px;
+    }
+    .jumbotron {
+        margin: 0px;
+    }
+    .categoryDeck {
+        padding: 20px;
+    }
+    div.row {
+        justify-content: space-evenly;
+    }
 </style>
--- src/views/Barn.vue
+++ src/views/Barn.vue
@@ -1,7 +1,14 @@
 <template>
-  <div>
-    <h1>Barn</h1>
-  </div>
+    <div>
+        <h1>Barn</h1>
+        <product-gallery></product-gallery>
+    </div>
 </template>
 
+<script>
+    import ProductGallery from '@/components/ProductGallery.vue'
+    export default {
+        components: { ProductGallery }
+    }
+</script>
 <style scoped lang="scss"></style>
