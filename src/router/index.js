90d86d4368f4a04b9796fcccaad21219d73905e2
Noel Andersson <noel.anderssonkohler@hotmail.com>
2021-02-24 20:46:22 +0100

    Started building the Checkout-page

--- src/router/index.js
+++ src/router/index.js
@@ -1,72 +1,77 @@
 // jshint esversion:6
 
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
-import Dam from "../views/Dam.vue";
-import Barn from "../views/Barn.vue";
-import Herr from "../views/Herr.vue";
-import ProductPage from "../views/ProductPage.vue";
-import About from "../views/About.vue";
-import Login from "../views/Login.vue";
-import Register from "../views/Register.vue";
-import User from "../views/User.vue";
-
-Vue.use(VueRouter);
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Dam from '../views/Dam.vue'
+import Barn from '../views/Barn.vue'
+import Herr from '../views/Herr.vue'
+import ProductPage from '../views/ProductPage.vue'
+import About from '../views/About.vue'
+import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
+import User from '../views/User.vue'
+import Checkout from '../views/Checkout.vue'
+Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/dam",
-    name: "Dam",
-    component: Dam
-  },
-  {
-    path: "/barn",
-    name: "Barn",
-    component: Barn
-  },
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/dam',
+        name: 'Dam',
+        component: Dam
+    },
+    {
+        path: '/barn',
+        name: 'Barn',
+        component: Barn
+    },
 
-  {
-    path: "/herr",
-    name: "Herr",
-    component: Herr
-  },
+    {
+        path: '/herr',
+        name: 'Herr',
+        component: Herr
+    },
 
-  {
-    path: "/product",
-    name: "Herr",
-    component: ProductPage
-  },
+    {
+        path: '/product',
+        name: 'Herr',
+        component: ProductPage
+    },
 
-  {
-    path: "/about",
-    name: "About",
-    component: About
-  },
-  {
-    path: "/login",
-    name: "Login",
-    component: Login
-  },
-  {
-    path: "/register",
-    name: "Register",
-    component: Register
-  },
-  {
-    path: "/user",
-    name: "User",
-    component: User
-  }
-];
+    {
+        path: '/about',
+        name: 'About',
+        component: About
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login
+    },
+    {
+        path: '/register',
+        name: 'Register',
+        component: Register
+    },
+    {
+        path: '/user',
+        name: 'User',
+        component: User
+    },
+    {
+        path: '/checkout',
+        name: 'Checkout',
+        component: Checkout
+    }
+]
 
 const router = new VueRouter({
-  routes
-});
+    routes
+})
 
-export default router;
+export default router
