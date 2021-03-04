9bd9798adf69308c3c8c6532ae138ff80c3af6d2
Hassan97-js <hassansyr58@gmail.com>
2021-03-04 15:40:07 +0000

    Caching Niclas, Hassan, and Noel's images in aboutPage

--- vue.config.js
+++ vue.config.js
@@ -21,10 +21,24 @@ module.exports = {
           },
           urlPattern:
             "https://media-exp1.licdn.com/dms/image/C4E03AQFDjNUPdMmmDg/profile-displayphoto-shrink_800_800/0/1614268833081?e=1620259200&v=beta&t=haI37MHYEnGSX4H97W_QBO8Hi1-_-iU_l8JBLmubKIc"
+        },
+        {
+          handler: "NetworkFirst",
+          options: {
+            networkTimeoutSeconds: 5
+          },
+          urlPattern:
+            "https://www.linkedin.com/in/niclas-lundstr%C3%B6m-62b9891b5/"
+        },
+        {
+          handler: "NetworkFirst",
+          options: {
+            networkTimeoutSeconds: 5
+          },
+          urlPattern: "http://placekitten.com/250/250"
         }
       ]
     },
-    publicPath: process.env.NODE_ENV === "production" ? "/dist/" : "/",
-    exclude: [ /_redirects/ ]
+    publicPath: process.env.NODE_ENV === "production" ? "/dist/" : "/"
   }
 };
